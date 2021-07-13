c850a74eac04d6419327f39b20283d48160b72b7
justengel <jtengel08@gmail.com>
2021-07-13 17:18:40 -0400

    Updated to version 1.0.0

--- module_path/__meta__.py
+++ module_path/__meta__.py
@@ -1,5 +1,5 @@
 name = 'module_path'
-version = '0.0.1'
+version = '1.0.0'
 description = 'Reliably find a modules path even when running an executable.'
 url = 'https://github.com/justengel/module_path'
 author = 'Justin Engel'
